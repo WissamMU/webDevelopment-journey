af2d50173c77e3914e9d267937b62cc374c4cdf7
WissamMU <wessam.2003.ali@gmail.com>
2024-04-16 00:52:01 +0300

    sidebar

--- youtuby/js/index.js
+++ youtuby/js/index.js
@@ -3,5 +3,17 @@ $(document).ready(function(){
         $(".nav-item").removeClass("active");
         $(this).addClass("active");
     });
-
+    $("#toggler").click(function(event) {
+        $('#wrap').toggleClass('toggled');
+        var left = $('.sidebar').css("left"); //أعطينا هذه الخاصية للقائمة الجانبية لكي تُصبح على اليمين
+        if (left == '0px') //القائمة الجانبية بحالة إظهار وبالتالي يجب إغلاقها
+        {
+            $('.sidebar').css({ 'left': '-17rem' });
+            $('.layer').fadeOut(); /*أخفي الطبقة السوداء*/
+        }
+        else { //القائمة الجانبية بحالة إغلاق وبالتالي يجب إظهارها
+            $('.sidebar').css({ 'left': '0' });
+            $('.layer').fadeIn(); 
+        }
+    });
 });
