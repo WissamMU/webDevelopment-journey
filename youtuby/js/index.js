0828a0e1c509e0e5b6e208ad0d29a5263aedca4c
WissamMU <wessam.2003.ali@gmail.com>
2024-04-22 06:02:40 +0300

    watch log js and style

--- youtuby/js/index.js
+++ youtuby/js/index.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    $(".first-list-element").addClass("active");
     $(".nav-item").click(function(){
         $(".nav-item").removeClass("active");
         $(this).addClass("active");
---
+++ youtuby/js/watch.js
@@ -0,0 +1,24 @@
+$(document).ready(function () {
+    $("#myBtn").on("click", function(){
+        read();
+    });
+    $(".reply").click(function() {
+        $(this).parents("div.row").next("div.card").toggle();
+    });
+});
+
+function read() {
+    var dots = document.getElementById("dots");
+    var moreText = document.getElementById("more");
+    var btnText = document.getElementById("myBtn");
+  
+    if (dots.style.display === "none") {
+      dots.style.display = "inline";
+      btnText.innerHTML = "show more";
+      moreText.style.display = "none";
+    } else {
+      dots.style.display = "none";
+      btnText.innerHTML = "show less";
+      moreText.style.display = "inline";
+    }
+}
