9914576211b235ba6875918feadada4f5a711d62
WissamMU <wessam.2003.ali@gmail.com>
2024-06-02 06:26:57 +1400

    main - contacts finshed

--- landing-pages/agency/js/index.js
+++ landing-pages/agency/js/index.js
@@ -1,15 +1,15 @@
-function scrollFunction(){
-    if(document.documentElement.scrollTop>200){
+function scrollFunction() {
+    if (document.documentElement.scrollTop > 200) {
         document.getElementById("navbar").classList.add("noTransparrent");
-    }else{
+    } else {
         document.getElementById("navbar").classList.remove("noTransparrent");
     }
 }
 
-window.onscroll =function(){
+window.onscroll = function () {
     scrollFunction();
 }
-window.onload = function(){
+window.onload = function () {
     let website = new CountUp('website-count', 0, 250, 0, 2.5);
     website.start();
     let app = new CountUp('app-count', 0, 100, 0, 2.5);
@@ -19,3 +19,23 @@ window.onload = function(){
     let video = new CountUp('video-count', 0, 30, 0, 2.5);
     video.start();
 };
+
+(function () {
+    'use strict'
+
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault()
+                    event.stopPropagation()
+                }
+
+                form.classList.add('was-validated')
+            }, false)
+        })
+})();
