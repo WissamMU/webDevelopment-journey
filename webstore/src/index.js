540a1993887d75dd96ee063e378f8c36d4638510
WissamMU <wessam.2003.ali@gmail.com>
2024-06-24 03:02:28 +0300

    font awsome and navbar

--- webstore/src/index.js
+++ webstore/src/index.js
@@ -1,5 +1,9 @@
-import "bootstrap/dist/js/bootstrap.min.js";
+window.bootstrap = require('bootstrap/dist/js/bootstrap.bundle.js');
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/stylesheet.css";
+import '@fortawesome/fontawesome-free/js/all.min';
+
+document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
+
 
 console.log("hi");
