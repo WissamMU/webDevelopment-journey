c351c92597b89905a61b935c25e59193f98d2ffe
WissamMU <wessam.2003.ali@gmail.com>
2024-06-27 19:30:49 +0300

    store featurs

--- src/index.js
+++ src/index.js
@@ -6,4 +6,10 @@ import '@fortawesome/fontawesome-free/js/all.min';
 document.querySelectorAll('[data-bs-toggle="tooltip"]').forEach(item => new bootstrap.Tooltip(item))
 
 
+    document.querySelectorAll('.add-to-cart-btn').forEach(item => {
+        item.addEventListener("click", () => {
+            alert("أضيف المُنتج إلى عربة الشراء")
+        })
+    })
+
 console.log("hi");
