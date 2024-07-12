828fd81b92d035376ae86f00482142120d19061a
WissamMU <wessam.2003.ali@gmail.com>
2024-07-11 23:33:33 -1200

    product page - product photo and option

--- webstore/src/index.js
+++ webstore/src/index.js
@@ -12,14 +12,34 @@ document.querySelectorAll('.add-to-cart-btn').forEach(item => {
     })
 })
 
-document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear();
+
+
+
+document.querySelectorAll('.size-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.size-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+
+document.querySelectorAll('.color-option input[type="radio"]').forEach(item => {
+    item.addEventListener('change', () => {
+        document.querySelectorAll('.color-option').forEach(i => {
+            i.classList.remove('active')
+        })
+        item.parentNode.parentNode.classList.add('active')
+    })
+})
+
 
 (() => {
     'use strict'
-
+    
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     const forms = document.querySelectorAll('.needs-validation')
-
+    
     // Loop over them and prevent submission
     Array.from(forms).forEach(form => {
         form.addEventListener('submit', event => {
@@ -27,8 +47,11 @@ document.getElementById("copyright").innerHTML = "جميع الحقوق محفو
                 event.preventDefault()
                 event.stopPropagation()
             }
-
+            
             form.classList.add('was-validated')
         }, false)
     })
-})()
+
+})();
+
+document.getElementById("copyright").innerHTML = "جميع الحقوق محفوظة للمتجر سنة " + new Date().getFullYear();
